3a9ffb1bf9e2a96fdd744a097494d04f4f1f097d
Akilesh Subhadu <86011577+Yohan4@users.noreply.github.com>
2024-03-30 17:38:07 +0400

    refactor: Update file structure and checkNumberOfArguments function implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "validation.h"
+#include "audiolibrary.h"
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -8,15 +9,40 @@
 #include <vector>
 
 
+/*
+    main.cpp
+    Authors: M00851681,
+    Created: 
+    Updated: 
+*/
+
+
 void clearScreen(const std::string& message)
 {
     std::cout << "\033[2J\033[1;1H";
     std::cout << message << std::endl;
 }
 
-void mainMenu() {
+bool checkNumberOfArguments(int& argumentsCount, char* argv[], std::string& filename)
+{
+    if (argumentsCount != 2)
+    {
+        std::cerr << "Invalid number of arguments." << std::endl;
+        std::cerr << "Program Execution: " << argv[0] << " <filename>" << std::endl;
+        std::cout << "Please enter the filename: ";
+        std::getline(std::cin, filename);
+        argumentsCount = 2; 
+        return true;
+    }
+    filename = argv[1];
+    return true;
+}
+
+
+void mainMenu() 
+{
     std::cout << std::endl;
-    clearScreen("Menu\n");
+    clearScreen("\n");
     std::cout << std::endl;
     std::cout << "*=============================================*" << std::endl;
     std::cout << "|                  MAIN MENU                  |" << std::endl;
@@ -33,49 +59,65 @@ void mainMenu() {
     std::cout << "*=============================================*" << std::endl;
 }
 
-int main()
+
+void userChoice(int choice, AudioLibrary& library)
+{
+    if (choice == 1)
+    {
+        library.userAddTrack();
+    }
+    else if (choice == 2)
+    {
+       
+    }
+    else if (choice == 3)
+    {
+        
+    }
+    else if (choice == 4)
+    {
+        
+    }
+    else if (choice == 5)
+    {
+        
+    }
+    else if (choice == 6)
+    {
+        std::cout << "Exiting the program. Goodbye!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Invalid choice. Please try again." << std::endl;
+    }
+}
+
+int main(int argc, char* argv[])
 {
     std::cout << std::endl << "Audio Track Library Management System" << std::endl
               << "Group Coursework" << std::endl
               << std::endl;
 
+    std::string filename;
+    int argumentCount = argc;
+
+    if (!checkNumberOfArguments(argumentCount, argv, filename))
+    {
+        return 1;
+    }
+
+    AudioLibrary library;
     int choice;
+
     do
     {
         mainMenu();
         choice = getValidChoice();
-
-        if (choice == 1)
-        {
-            
-        }
-        else if (choice == 2)
-        {
-    
-        }
-        else if (choice == 3)
-        {
-
-        }
-        else if (choice == 4)
-        {
-            
-        }
-        else if (choice == 5)
-        {
-            
-        }
-        else if (choice == 6)
-        {
-             std::cout << "Exiting the program. Goodbye!" << std::endl;
-        }
-        else
-        {
-            std::cout << "Invalid choice. Please try again." << std::endl;
-        }
+        userChoice(choice, library);
         std::cout << std::endl;
     } while (choice != 6);
 
     return 0;
 }
+
 
