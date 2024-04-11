be3ceee7f9b6d6e2bf51e6ed5fc73e3e3a080154
Akilesh Subhadu <86011577+Yohan4@users.noreply.github.com>
2024-04-11 14:12:58 +0400

    feat: Implement  the  hashTable functionalities for add and delete

---
+++ src/hashtable.cpp
@@ -0,0 +1,56 @@
+#include "hashtable.hpp"
+#include "audiotrack.hpp"
+#include <algorithm>
+
+
+template <typename Key, typename Value>
+HashTable<Key, Value>::HashTable(size_t size) : tableSize(size) {
+    table.resize(size);
+}
+
+
+template <typename key, typename Value>
+HashTable<key, Value> ::~HashTable(){
+
+}
+
+// compute hash value
+template <typename Key, typename Value>
+size_t HashTable<Key, Value>::hash(const Key& key) {
+    std::hash<Key> hasher;
+    return hasher(key) % tableSize;
+}
+
+template <typename Key, typename Value>
+void HashTable<Key, Value>::insert(const Key& key, const Value& value){
+    size_t index = hash(key);
+    std::list<std::pair<Key, Value>>& bucket = table[index];
+
+    // Update exiting entry
+    for (std::pair<Key, Value>& element : bucket) {
+        if (element.first == key) {
+            element.second = value;
+            return;
+        }
+    }
+    // Add new key-value pair
+    bucket.emplace_back(key, value);
+}
+
+// Delete audio
+template <typename Key, typename Value>
+bool HashTable<Key, Value>::remove(const Key& key) {
+    size_t index = hash(key);
+    std::list<std::pair<Key, Value>>& bucket = table[index];
+
+    for (auto& entry : bucket) {
+        if (entry.first == key) {
+            bucket.remove(entry);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+template class HashTable<std::string, AudioTrack>;
