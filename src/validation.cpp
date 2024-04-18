ae58da282ef2606c492cb0f8323e4ce9017a29d9
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-04-18 18:42:20 +0400

    feat: removed validAddChoice()

--- src/validation.cpp
+++ src/validation.cpp
@@ -30,29 +30,3 @@ int getValidChoice() {
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   return choice;
 }
-
-
-int getValidAddChoice() {
-  int addChoice;
-  bool valid = false;
-
-  do {
-    std::cout << "\n1.Add from CSV \n2.Add manually \n\nEnter your choice: ";
-    std::cin >> addChoice;
-
-    if (std::cin.fail()) {
-      std::cout << "\nInvalid Input! Please enter a valid integer.\n" 
-                << std::endl;
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    } else if (addChoice != 1 && addChoice != 2) {
-        std::cout << "\nInvalid Choice! Please enter 1 or 2.\n" 
-                  << std::endl;
-    } else {
-        valid = true;
-    }
-  } while (!valid);
-
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  return addChoice;
-}
