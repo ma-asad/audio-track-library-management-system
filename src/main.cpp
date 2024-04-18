7fe68095a06c38d9da742d710bc1e1c596703df7
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-04-18 18:42:08 +0400

    refactor: changed main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
     Created:
     Updated:
 */
+
+// TODO: move enter() and clearScreen() to a separate file (utils.hpp)
 void enter() {
   std::cout << "\nPress 'Enter' to go back to main menu";
   std::cin.get();
@@ -23,41 +25,54 @@ void mainMenu() {
   std::cout << "|                  MAIN MENU                  |" << std::endl;
   std::cout << "*=============================================*" << std::endl;
   std::cout << "| Choose an option below :                    |" << std::endl;
-  std::cout << "| 1. Add Music File                           |" << std::endl;
-  std::cout << "| 2. Delete                                   |" << std::endl;
+  std::cout << "| 1. Add Audio File                           |" << std::endl;
+  std::cout << "| 2. List Audio                               |" << std::endl;
   std::cout << "| 3. Search                                   |" << std::endl;
   std::cout << "| 4. Create Playlist                          |" << std::endl;
-  std::cout << "| 5. List Audio                               |" << std::endl;
+  std::cout << "| 5. Delete                                   |" << std::endl;
   std::cout << "| 6. Exit                                     |" << std::endl;
   std::cout << "*=============================================*" << std::endl;
+  std::cout << std::endl;
 }
 
 void userChoice(int choice, AudioLibrary& library) {
   std::string audioName;
   if (choice == 1) {
     clearScreen("Add: Users can add new audio tracks \n");
-    int addChoice = getValidAddChoice();
-    if (addChoice == 1) {
-      library.addTrackFromCSV();
-    } else if (addChoice == 2) {
-      library.addTrackManually();
-    }
+
+    library.addTrackFromCSV();
     enter();
+
   } else if (choice == 2) {
-    clearScreen("Delete: Users can delete existing audio tracks \n");
-    std::cout << "Enter the name of the audio track to delete: ";
-    std::getline(std::cin, audioName);
-    library.deleteTrack(audioName);
-    enter();
-  } else if (choice == 3) {
-    enter();
-  } else if (choice == 4) {
-    enter();
-  } else if (choice == 5) {
     clearScreen("List Audio: View a list of audio tracks & details");
+
+    // TODO: move below code to listAudio() function
     std::cout << std::endl;
     std::cout << "-----------------------------" << std::endl;
     library.listAudio();
+    enter();
+
+  } else if (choice == 3) {
+    clearScreen("Search: By Artist, Audio Title, Album, Genre, or Playlist \n");
+    // TODO: create sub-menu for search options
+    // 1. Artist
+    // 2. Audio Title
+    // 3. Album
+    // 4. Genre
+    // 5. Playlist
+    // 6. Back to main menu
+  
+  } else if (choice == 4) {
+    clearScreen("Create Playlist: By Audio Title \n");
+    // TODO: create a playlist function
+  
+  } else if (choice == 5) {
+    clearScreen("Delete: Users can delete existing audio tracks \n");
+
+    // TODO: move below code to deleteTrack() function
+    std::cout << "Enter the name of the audio track to delete: ";
+    std::getline(std::cin, audioName);
+    library.deleteTrack(audioName);
     enter();
   } else if (choice == 6) {
     std::cout << "Exiting the program. Goodbye!" << std::endl;
