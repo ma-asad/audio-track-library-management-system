18701aa3434c3620d1576b7747a75f27428ef47a
Seeraullee Ayman <m.seeraulleeayman@gmail.com>
2024-04-21 14:35:44 +0400

    test: added bool variable

--- test/test_mainmenu.cpp
+++ test/test_mainmenu.cpp
@@ -5,81 +5,88 @@
 #include "catch.hpp"
 #include "utils.hpp"
 
-TEST_CASE("Main Menu Selection")
-{
-    /**
-     * @brief This section tests the scenario when an invalid option is selected.
-     */
-    SECTION("Selecting invalid option")
-    {
-        AudioLibrary library;
-        bool stopProgram = false;
-        std::stringstream buffer;
-        std::streambuf *sbuf = std::cout.rdbuf();
-        std::cout.rdbuf(buffer.rdbuf());
+extern bool isTestEnvironment;
 
-        std::istringstream input("51\n6\n");
-        std::streambuf *oldCinBuf = std::cin.rdbuf();
-        std::cin.rdbuf(input.rdbuf());
+TEST_CASE("Main Menu Selection") {
+  isTestEnvironment = true;
 
-        mainMenuSelector(&stopProgram, library);
-        std::cin.rdbuf(oldCinBuf);
-        std::cout.rdbuf(sbuf);
-        std::string output = buffer.str();
-        REQUIRE(output.find("Invalid choice. Press 'Enter' to try again.") != std::string::npos);
-    }
+  /**
+   * @brief This section tests the scenario when an invalid option is selected.
+   */
+  SECTION("Selecting invalid option") {
+    AudioLibrary library;
+    bool stopProgram = false;
+    std::stringstream buffer;
+    std::streambuf *sbuf = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
 
-    /**
-     * @brief This section tests the scenario when the program is stopped.
-     */
-    SECTION("Stopping the program")
-    {
-        AudioLibrary library;
-        bool stopProgram = false;
-        std::stringstream buffer;
-        std::streambuf *sbuf = std::cout.rdbuf();
-        std::cout.rdbuf(buffer.rdbuf());
+    std::istringstream input("51\n6\n");
+    std::streambuf *oldCinBuf = std::cin.rdbuf();
+    std::cin.rdbuf(input.rdbuf());
 
-        std::istringstream input("6\n");
-        std::streambuf *oldCinBuf = std::cin.rdbuf();
-        std::cin.rdbuf(input.rdbuf());
+    mainMenuSelector(&stopProgram, library);
+    std::cin.rdbuf(oldCinBuf);
+    std::cout.rdbuf(sbuf);
+    std::string output = buffer.str();
+    REQUIRE(output.find("Invalid choice. Press 'Enter' to try again.") !=
+            std::string::npos);
+  }
 
-        mainMenuSelector(&stopProgram, library);
+  /**
+   * @brief This section tests the scenario when the program is stopped.
+   */
+  SECTION("Stopping the program") {
+    AudioLibrary library;
+    bool stopProgram = false;
+    std::stringstream buffer;
+    std::streambuf *sbuf = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
 
-        std::cin.rdbuf(oldCinBuf);
-        std::cout.rdbuf(sbuf);
-        std::string output = buffer.str();
+    std::istringstream input("6\n");
+    std::streambuf *oldCinBuf = std::cin.rdbuf();
+    std::cin.rdbuf(input.rdbuf());
 
-        REQUIRE(output.find("Exiting the program. Goodbye!") != std::string::npos);
-        REQUIRE(stopProgram == true);
-    }
+    mainMenuSelector(&stopProgram, library);
 
-    /**
-     * @brief This section tests if the main menu is correctly displayed.
-     */
-    SECTION("Checking if menu is correctly displayed")
-    {
-        AudioLibrary library;
-        bool stopProgram = false;
-        std::stringstream buffer;
-        std::streambuf *sbuf = std::cout.rdbuf();
-        std::cout.rdbuf(buffer.rdbuf());
+    std::cin.rdbuf(oldCinBuf);
+    std::cout.rdbuf(sbuf);
+    std::string output = buffer.str();
 
-        std::istringstream input("6");
-        std::streambuf *oldCinBuf = std::cin.rdbuf();
-        std::cin.rdbuf(input.rdbuf());
+    REQUIRE(output.find("Exiting the program. Goodbye!") != std::string::npos);
+    REQUIRE(stopProgram == true);
+  }
 
-        mainMenuSelector(&stopProgram, library);
+  /**
+   * @brief This section tests if the main menu is correctly displayed.
+   */
+  SECTION("Checking if menu is correctly displayed") {
+    AudioLibrary library;
+    bool stopProgram = false;
+    std::stringstream buffer;
+    std::streambuf *sbuf = std::cout.rdbuf();
+    std::cout.rdbuf(buffer.rdbuf());
 
-        std::cin.rdbuf(oldCinBuf);
-        std::cout.rdbuf(sbuf);
-        std::string output = buffer.str();
+    std::istringstream input("6");
+    std::streambuf *oldCinBuf = std::cin.rdbuf();
+    std::cin.rdbuf(input.rdbuf());
 
-        REQUIRE(output.find("| 1. Add Audio File                           |") != std::string::npos);
-        REQUIRE(output.find("| 2. List Audio                               |") != std::string::npos);
-        REQUIRE(output.find("| 3. Search                                   |") != std::string::npos);
-        REQUIRE(output.find("| 4. Create Playlist                          |") != std::string::npos);
-        REQUIRE(output.find("| 5. Delete                                   |") != std::string::npos);
-        REQUIRE(output.find("| 6. Exit                                     |") != std::string::npos);
-    }
+    mainMenuSelector(&stopProgram, library);
+
+    std::cin.rdbuf(oldCinBuf);
+    std::cout.rdbuf(sbuf);
+    std::string output = buffer.str();
+
+    REQUIRE(output.find("| 1. Add Audio File                           |") !=
+            std::string::npos);
+    REQUIRE(output.find("| 2. List Audio                               |") !=
+            std::string::npos);
+    REQUIRE(output.find("| 3. Search                                   |") !=
+            std::string::npos);
+    REQUIRE(output.find("| 4. Create Playlist                          |") !=
+            std::string::npos);
+    REQUIRE(output.find("| 5. Delete                                   |") !=
+            std::string::npos);
+    REQUIRE(output.find("| 6. Exit                                     |") !=
+            std::string::npos);
+  }
 }
